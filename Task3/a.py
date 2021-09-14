846f6f326e3d2befd7308619631fa223c372bc99
Martin Hagen <martin@partins.no>
2021-09-14 17:34:43 +0200

    Simplified task a to c

--- Task3/a.py
+++ Task3/a.py
@@ -1,28 +1,7 @@
 import torch
 import torch.nn as nn
-import torchvision
 
-# Load observations from the mnist dataset. The observations are divided into a training set and a test set
-mnist_train = torchvision.datasets.MNIST('./data', train=True, download=True)
-x_train = mnist_train.data.reshape(-1, 1, 28, 28).float()  # torch.functional.nn.conv2d argument must include channels (1)
-y_train = torch.zeros((mnist_train.targets.shape[0], 10))  # Create output tensor
-y_train[torch.arange(mnist_train.targets.shape[0]), mnist_train.targets] = 1  # Populate output
-
-mnist_test = torchvision.datasets.MNIST('./data', train=False, download=True)
-x_test = mnist_test.data.reshape(-1, 1, 28, 28).float()  # torch.functional.nn.conv2d argument must include channels (1)
-y_test = torch.zeros((mnist_test.targets.shape[0], 10))  # Create output tensor
-y_test[torch.arange(mnist_test.targets.shape[0]), mnist_test.targets] = 1  # Populate output
-
-# Normalization of inputs
-mean = x_train.mean()
-std = x_train.std()
-x_train = (x_train - mean) / std
-x_test = (x_test - mean) / std
-
-# Divide training data into batches to speed up optimization
-batches = 600
-x_train_batches = torch.split(x_train, batches)
-y_train_batches = torch.split(y_train, batches)
+import run_model
 
 
 class ConvolutionalNeuralNetworkModel(nn.Module):
@@ -50,14 +29,5 @@ class ConvolutionalNeuralNetworkModel(nn.Module):
         return torch.mean(torch.eq(self.f(x).argmax(1), y.argmax(1)).float())
 
 
-model = ConvolutionalNeuralNetworkModel()
-
-# Optimize: adjust W and b to minimize loss using stochastic gradient descent
-optimizer = torch.optim.Adam(model.parameters(), 0.001)
-for epoch in range(20):
-    for batch in range(len(x_train_batches)):
-        model.loss(x_train_batches[batch], y_train_batches[batch]).backward()  # Compute loss gradients
-        optimizer.step()  # Perform optimization by adjusting W and b,
-        optimizer.zero_grad()  # Clear gradients for next step
-
-    print("accuracy = %s" % model.accuracy(x_test, y_test))
+if __name__ == "__main__":
+    run_model.run_model(ConvolutionalNeuralNetworkModel())
