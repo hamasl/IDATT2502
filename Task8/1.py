78c3e704264f21ef6bcaa73423bb1c388c5c3fb5
Martin Hagen <martin@partins.no>
2021-10-19 22:11:36 +0200

    Completed task 8, question 1

--- Task8/1.py
+++ Task8/1.py
@@ -1,21 +1,22 @@
-# Inspired by: https://github.com/JackFurby/CartPole-v0
+# NOTE: Inspired by: https://github.com/JackFurby/CartPole-v0
 import gym
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 # TODO create bins for the observations
 class Bucketed_Q_Table():
     def __init__(self, observation_space, action_space, num_of_buckets):
-        #self._buckets = self._create_buckets(observation_space, num_of_buckets)
-        self._buckets = self._create_buckets_v2()
+        self._buckets = self._create_buckets(observation_space, num_of_buckets)
+        #self._buckets = self._create_buckets_v2()
         self._table = np.zeros(([num_of_buckets] * len(observation_space.high) + [action_space.n]))
         print(self._buckets)
-        #print(self._table.shape)
+        # print(self._table.shape)
 
     def _create_buckets(self, observation_space, num_of_buckets):
         num_of_observations = len(observation_space.high)
-        #print(observation_space.high)
-        #print(observation_space.low)
+        # print(observation_space.high)
+        # print(observation_space.low)
         bucks = []
         for i in range(num_of_observations):
             # Since a bucket contains values between linspace entries the linspace needs one more element than the number of buckets
@@ -35,7 +36,7 @@ class Bucketed_Q_Table():
         ]
 
     def _get_bucket_index(self, observation_num, observation):
-        for i in range(len(self._buckets[observation_num])-1):
+        for i in range(len(self._buckets[observation_num]) - 1):
             if i == 20:
                 print(observation)
                 print(observation_num)
@@ -58,17 +59,12 @@ class Bucketed_Q_Table():
 
     def set_q_value(self, observations, action_index, q_value):
         self._get_action_q_values(observations)[action_index] = q_value
-        # self._table[observation_index, self._get_bucket_index(observation_index, observations), action_index] = q_value
 
     def get_q_value(self, observations, action_index):
         return self._get_action_q_values(observations)[action_index]
-        # return self._table[observation_index, self._get_bucket_index(observation_index, observation), action_index]
 
     def print_table(self):
         print(self._table)
-
-    def get_max(self):
-        return np.max(self._table)
 
 
 def run_finished(q_table, k):
@@ -83,49 +79,42 @@ def run_finished(q_table, k):
             observation, reward, done, info = env.step(action)
     env.close()
 
+
 if __name__ == '__main__':
     NUM_OF_BUCKETS = 20
     NUM_OF_EPISODES = 10000
     DISCOUNT_FACTOR = 0.95
     LEARNING_RATE = 0.1
-    eps = lambda episode: 0.3*(NUM_OF_EPISODES-episode)
+
+    eps = lambda episode: (NUM_OF_EPISODES - episode) / NUM_OF_EPISODES
 
     env = gym.make("CartPole-v0")
 
     q_table = Bucketed_Q_Table(env.observation_space, env.action_space, NUM_OF_BUCKETS)
-    """""
-    """""
-    #q_table.print_table()
+
     epochs_run_tbl = []
     for episode_num in range(NUM_OF_EPISODES):
         done = False
         observation = env.reset()
         epochs_run = 0
-        #TODO not using new and old state
         while not done:
-            #env.render()
-            # TODO fix if
             if np.random.uniform(0, 1) < eps(episode_num):
                 action = env.action_space.sample()
             else:
                 action = q_table.optimal_choice(observation)
             new_observation, reward, done, info = env.step(action)
-
+            if done and epochs_run < 150:
+                reward = -200
             current_q = q_table.get_q_value(observation, action)
             max_future_q = q_table.optimal_val(new_observation)
-            # TODO this formula is technically not right
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT_FACTOR * max_future_q)
             q_table.set_q_value(observation, action, new_q)
             observation = new_observation
             epochs_run += 1
-            #print(action)
-            # print(current_q)
-            #print(max_q)
-            #print(new_q)
         if episode_num % 100 == 0:
             print(f"Episode num: {episode_num}, epochs run: {epochs_run},")
-            print(q_table.get_max())
         epochs_run_tbl.append(epochs_run)
-    q_table.print_table()
     env.close()
+    plt.plot(epochs_run_tbl, label="Epochs run for each episode")
+    plt.show()
     run_finished(q_table, 20)
