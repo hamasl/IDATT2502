acacb37798438394b63514848cfc0f638503f0d2
Martin Hagen <martin@partins.no>
2021-10-21 19:57:23 +0200

    Solved most of task 2

--- Task8/2.py
+++ Task8/2.py
@@ -6,16 +6,17 @@ from matplotlib import pyplot as plt
 
 
 def get_random_pos(dimension):
-    return {"x": np.random.randint(0, dimension), "y": np.random.randint(0, dimension)}
+    return np.random.randint(0, dimension), np.random.randint(0, dimension)
 
 
 class GridWorld:
     def __init__(self, block_size, dimension, finish_pos):
         self._dimension = dimension
-        self.finish_pos = finish_pos
+        self._finish_pos = finish_pos
         self._block_size = block_size
-        self.pos = get_random_pos(dimension)
-        self.actions = [
+        self._pos = get_random_pos(dimension)
+        self._prev_pos = None
+        self._actions = [
             self._up,
             self._left,
             self._down,
@@ -23,26 +24,27 @@ class GridWorld:
         ]
         self._tiles = []
         self._grid = self._create_grid()
+        print(self._tiles[1][1])
 
     def _update_pos(self):
         pygame.draw.circle(self._grid, pygame.Color(""))
         pygame.display.flip()
 
     def _up(self):
-        if self.pos["y"] > 0:
-            self.pos["y"] -= 1
+        if self._pos[0] > 0:
+            self._pos = self._pos[0] - 1, self._pos[1]
 
     def _left(self):
-        if self.pos["x"] > 0:
-            self.pos["x"] -= 1
+        if self._pos[1] > 0:
+            self._pos = self._pos[0], self._pos[1] - 1
 
     def _down(self):
-        if self.pos["y"] < self._dimension - 1:
-            self.pos["y"] += 1
+        if self._pos[0] < self._dimension - 1:
+            self._pos = self._pos[0] + 1, self._pos[1]
 
     def _right(self):
-        if self.pos["x"] < self._dimension - 1:
-            self.pos["x"] += 1
+        if self._pos[1] < self._dimension - 1:
+            self._pos = self._pos[0], self._pos[1] + 1
 
     def _create_grid(self):
         surface = pygame.display.set_mode((self._block_size * self._dimension, self._block_size * self._dimension))
@@ -54,60 +56,77 @@ class GridWorld:
                 pygame.draw.rect(surface, pygame.Color("grey"),
                                  pygame.Rect(j * self._block_size, i * self._block_size, self._block_size,
                                              self._block_size), 0)
-                if j == self.finish_pos["x"] and i == self.finish_pos["y"]:
-                    row.append(pygame.draw.rect(surface, pygame.Color("green"),
-                                                pygame.Rect(j * self._block_size + 2, i * self._block_size + 2,
-                                                            self._block_size - 4,
-                                                            self._block_size - 4)))
-                else:
-                    row.append(pygame.draw.rect(surface, pygame.Color("black"),
-                                                pygame.Rect(j * self._block_size + 2, i * self._block_size + 2,
-                                                            self._block_size - 4,
-                                                            self._block_size - 4)))
+                color = "black"
+                if i == self._finish_pos[0] and j == self._finish_pos[1]:
+                    color = "green"
+                row.append((pygame.draw.rect(surface, pygame.Color(color),
+                                            pygame.Rect(j * self._block_size + 2, i * self._block_size + 2,
+                                                        self._block_size - 4,
+                                                        self._block_size - 4)), color))
             self._tiles.append(row)
         print(self._tiles)
         pygame.display.flip()
         pygame.display.set_caption("Gridworld")
         return surface
 
-    def render(self, old_pos=None):
+    def get_random_action(self):
+        return np.random.randint(0, len(self._actions))
+
+    def num_of_actions(self):
+        return len(self._actions)
+
+    def render(self,):
         pygame.draw.circle(self._grid, pygame.Color("blue"),
-                           (self._block_size * (self.pos["x"] + 0.5), self._block_size * (self.pos["y"] + 0.5)), 20)
-        if old_pos is not None:
-            color = self._grid[old_pos["y"]][old_pos["x"]].color
-            pygame.draw.circle(self._grid, color, (self._block_size * (old_pos["x"] + 0.5), self._block_size * (old_pos["y"] + 0.5)), 20)
+                           (self._block_size * (self._pos[1] + 0.5), self._block_size * (self._pos[0] + 0.5)), 20)
+        if self._prev_pos is not None:
+            color = self._tiles[self._prev_pos[0]][self._prev_pos[1]][1]
+            pygame.draw.circle(self._grid, color,
+                               (self._block_size * (self._prev_pos[1] + 0.5), self._block_size * (self._prev_pos[0] + 0.5)), 20)
         pygame.display.flip()
 
     """Reward is the return value"""
 
     def step(self, action):
-        old_pos = self.pos.copy()
-        self.actions[action]()
-        self._render_step(old_pos)
+        self._prev_pos = self._pos
+        self._actions[action]()
         # If the game is done or not
-        if self.pos == self.finish_pos:
-            return 1, True
+        if self._pos == self._finish_pos:
+            return self._pos, 1, True
         else:
-            return -1, False
+            return self._pos, -1, False
 
     def reset(self):
-        self.pos = get_random_pos(self._dimension)
+        self._pos = get_random_pos(self._dimension)
+        return self._pos
 
+
+def run_finished(q_table, k, block_size, dimension, finish_pos):
+    env = GridWorld(block_size, dimension, finish_pos)
+    for episode_num in range(k):
+        done = False
+        observation = env.reset()
+        while not done:
+            env.render()
+            action = np.argmax(q_table[observation])
+            observation, reward, done = env.step(action)
+            time.sleep(0.3)
 
 if __name__ == '__main__':
 
-    NUM_OF_BUCKETS = 20
-    NUM_OF_EPISODES = 10000
+    NUM_OF_EPISODES = 30000
     DISCOUNT_FACTOR = 0.95
     LEARNING_RATE = 0.1
 
     eps = lambda episode: max((NUM_OF_EPISODES - 2 * episode) / NUM_OF_EPISODES, 0)
 
-    env = GridWorld(50, 8, {"x": 2, "y": 3})
+    DIMENSION = 8
+    BLOCK_SIZE = 50
+    FINISH_POS = (3, 2)
+    env = GridWorld(50, DIMENSION, FINISH_POS)
     pygame.init()
 
-    #TODO implement proper q-table
-    q_table = []
+    # TODO implement proper q-table
+    q_table = np.zeros([DIMENSION] * 2 + [env.num_of_actions()])
 
     epochs_run_tbl = []
     for episode_num in range(NUM_OF_EPISODES):
@@ -116,22 +135,24 @@ if __name__ == '__main__':
         epochs_run = 0
         while not done:
             if np.random.uniform(0, 1) < eps(episode_num):
-                action = env.action_space.sample()
+                action = env.get_random_action()
             else:
-                action = q_table.optimal_choice(observation)
-            new_observation, reward, done, info = env.step(action)
-            if done and epochs_run < 150:
-                reward = -200
-            current_q = q_table.get_q_value(observation, action)
-            max_future_q = q_table.optimal_val(new_observation)
+                action = np.argmax(q_table[observation])
+            new_observation, reward, done = env.step(action)
+            current_q = q_table[observation + (action,)]
+            max_future_q = np.max(q_table[new_observation])
             new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT_FACTOR * max_future_q)
-            q_table.set_q_value(observation, action, new_q)
+            #print(q_table[observation])
+            #print(q_table[observation + (action,)])
+            #print(new_q)
+            q_table[observation + (action,)] = new_q
             observation = new_observation
             epochs_run += 1
         if episode_num % 100 == 0:
             print(f"Episode num: {episode_num}, epochs run: {epochs_run},")
         epochs_run_tbl.append(epochs_run)
+
     plt.plot(epochs_run_tbl, label="Epochs run for each episode")
     plt.show()
-    run_finished(q_table, 20)
+    run_finished(q_table, 20, BLOCK_SIZE, DIMENSION, FINISH_POS)
     pygame.quit()
